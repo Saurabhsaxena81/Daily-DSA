a4f24d985cdb184b7e45cd62dbd51250e35e423e
Saurabhsaxena81 <saurabhsaxena81272@gmail.com>
2026-03-06 09:49:29 +0530

    6th march 2026

---
+++ LeetCode/check-if-binary-string-has-at-most-one-segment-of-ones/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
+
+// time complexity O(n)
+// space complexity O(1)
+
+
+Smallest window containing all characters
