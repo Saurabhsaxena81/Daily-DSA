fa92414c7d8f7b1c2846438ffbc36c4410cf920d
Saurabhsaxena81 <saurabhsaxena81272@gmail.com>
2026-03-18 10:39:54 +0530

    18th march 2026

---
+++ GFG/Distribute Candies/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int solve(Node root,int moves[]){
+        if(root == null) return 0;
+        
+        int l = solve(root.left,moves);
+        int r = solve(root.right,moves);
+        int totalCan=(l+r+root.data)-1;
+        moves[0] += Math.abs(l)+Math.abs(r);
+        return totalCan;
+        
+    }
+    public int distCandy(Node root) {
+        // code here
+        if(root == null || (root.left == null && root.right == null)) return 0;
+        
+        int moves[]={0};
+        solve(root,moves);
+        return moves[0];
+    }
+}
